ac5f86defb7043af493ad74b2736ba121c289165
Cristopher Salazar <csalazara@ucenfotec.ac.cr>
2023-02-21 22:02:34 -0600

    NavBar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { AppBar } from "@mui/material";
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+
+const icon = {
+  margin: '15px',
+  fontSize: '60px'
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 const App = () => {
   return (
     <>
+    <AppBar color="#253237" >
+    <DirectionsCarIcon sx={icon}/>
+    </AppBar>
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<div>CARS</div>}/>
