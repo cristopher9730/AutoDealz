b46ed2b609798cbaf1dface04fa0c3088021d1ae
Sebastian Q <squirosv@ucenfotec.ac.cr>
2023-02-21 22:41:39 -0600

    theme

--- src/App.js
+++ src/App.js
@@ -3,34 +3,38 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import { AppBar } from "@mui/material";
+import { AppBar,createTheme,ThemeProvider} from "@mui/material";
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-
 const icon = {
   margin: '15px',
   fontSize: '60px'
 }
-
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
     primary: {
       main: '#1976d2',
+      contrastText:'#ffcc00'
     },
+    secondary:{
+      main: '#1976d2',
+      contrastText:'#ffcc00'
+    }
   },
 });
 
 const App = () => {
   return (
     <>
-    <AppBar color="#253237" >
-    <DirectionsCarIcon sx={icon}/>
-    </AppBar>
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<div>CARS</div>}/>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='primary' >
+      <DirectionsCarIcon sx={icon}/>
+      </AppBar>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<div>CARS</div>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
     </>
   );
 }
